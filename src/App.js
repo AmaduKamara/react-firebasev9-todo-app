42d0735d3247b343cd2c334582b42b863b7a08b4
Amkam <amaduskamara@gmail.com>
2022-10-19 13:08:21 +0000

    Add todo structure and basic styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import Todo from "./components/Todo";
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#477998] to-[#F64740]`,
+};
 
 function App() {
+  const [todos, setTodos] = useState([
+    "Learn react",
+    "Learn NodeJS for API development",
+  ]);
   return (
-    <div>
-      <h1>React Firebase-v9 Todo App</h1>
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h1 className={style.heading}>React Firebase-v9 Todo App</h1>
+        <form className={style.form}>
+          <input type="text" placeholder="Add todo" className={style.input} />
+          <button className={style.button}>
+            <AiOutlinePlus />
+          </button>
+        </form>
+        <ul>
+          {todos.map((todo, idx) => (
+            <Todo key={idx} todo={todo} />
+          ))}
+        </ul>
+        <p className={style.count}>You have {todos.length} todos</p>
+      </div>
     </div>
   );
 }
